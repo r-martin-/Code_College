d309df27d765c7d786abbbd2b2f4a97a47126bfb
r-martin- <c13730359@mydit.ie>
2015-01-31 23:17:10 +0000

    update

---
+++ Exercise_code/100_exercises/list_to_tuple.py
@@ -0,0 +1,22 @@
+i = 0
+
+tup1 = ()
+while i < 10:
+    number =input("enter a number")
+    #number = int(number)
+    numbers.append(number)
+    
+
+    i+=1
+    
+   
+
+#
+tup1 = numbers.split(',')
+
+
+
+
+print(numbers)
+
+    
