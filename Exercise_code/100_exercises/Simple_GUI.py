da45d8e29040afe37e9335e0d37130b64a1c87ff
r-martin- <c13730359@mydit.ie>
2015-04-08 21:25:29 +0100

    added

---
+++ Exercise_code/100_exercises/Simple_GUI.py
@@ -0,0 +1,25 @@
+"""
+simple GUI programe
+"""
+from tkinter import *
+class App():
+
+    def __init__ (self,master):
+	    frame = Frame(master)
+	    frame.pack()
+	    self.button = Button(frame,
+                                 text = "Quit", fg = "blue",
+                                 command = frame.quit)
+	    self.button.pack(side=LEFT)
+	    self.slogan= Button(frame,
+				 text = "Hello",
+				 command = self.write_slogan)
+	    self.slogan.pack(side = LEFT)
+    def write_slogan(self):
+	    print("it is easy to use")
+
+root = Tk()
+app = App(root)
+root.mainloop()
+
+    
